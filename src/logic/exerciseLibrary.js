2949a17b186fe611f74abb7b345a1fe7bfcb0a44
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-05-28 16:29:49 +0100

    Update exerciseLibrary.js

--- src/logic/exerciseLibrary.js
+++ src/logic/exerciseLibrary.js
@@ -1,1 +1,13 @@
+import { exercises } from "../data/exercises.js";
+import { store } from "../state/store.js";
 
+export function getAllExercises() {
+  return [
+    ...exercises,
+    ...(store.data.customExercises || [])
+  ];
+}
+
+export function getExerciseById(id) {
+  return getAllExercises().find(exercise => exercise.id === id);
+}
