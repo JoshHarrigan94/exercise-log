38c66e8789d02ad22759a6632469b6c7ce9a4f02
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-01 19:46:32 +0100

    Create index.js

---
+++ src/engine/programming/index.js
@@ -0,0 +1,23 @@
+import {
+  getProgrammeDNA,
+  getAllProgrammeDNA,
+  getProgrammeProgressionModel
+} from "./programmeDNA.js";
+
+import {
+  getRuleSetForProgramme,
+  getRuleSetForProgrammeMatch,
+  getAllProgressionRuleModels
+} from "./ruleSelector.js";
+
+import { makeCoachingDecision } from "./coachingDecision.js";
+
+export {
+  getProgrammeDNA,
+  getAllProgrammeDNA,
+  getProgrammeProgressionModel,
+  getRuleSetForProgramme,
+  getRuleSetForProgrammeMatch,
+  getAllProgressionRuleModels,
+  makeCoachingDecision
+};
