895924f9fec14dd19d65aa3c22a2c18e52776724
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-01 20:55:31 +0100

    Create expressionTypes.js

---
+++ src/data/expressionTypes.js
@@ -0,0 +1,83 @@
+export const expressionTypes = [
+
+  {
+    id: "max-strength",
+    name: "Max Strength"
+  },
+
+  {
+    id: "relative-strength",
+    name: "Relative Strength"
+  },
+
+  {
+    id: "hypertrophy",
+    name: "Hypertrophy"
+  },
+
+  {
+    id: "strength-endurance",
+    name: "Strength Endurance"
+  },
+
+  {
+    id: "work-capacity",
+    name: "Work Capacity"
+  },
+
+  {
+    id: "power",
+    name: "Power"
+  },
+
+  {
+    id: "reactive-strength",
+    name: "Reactive Strength"
+  },
+
+  {
+    id: "elasticity",
+    name: "Elasticity"
+  },
+
+  {
+    id: "speed",
+    name: "Speed"
+  },
+
+  {
+    id: "skill",
+    name: "Skill"
+  },
+
+  {
+    id: "motor-control",
+    name: "Motor Control"
+  },
+
+  {
+    id: "mobility",
+    name: "Mobility"
+  },
+
+  {
+    id: "tendon-capacity",
+    name: "Tendon Capacity"
+  },
+
+  {
+    id: "tissue-tolerance",
+    name: "Tissue Tolerance"
+  },
+
+  {
+    id: "aerobic-capacity",
+    name: "Aerobic Capacity"
+  },
+
+  {
+    id: "anaerobic-capacity",
+    name: "Anaerobic Capacity"
+  }
+
+];
