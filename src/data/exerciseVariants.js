4e5bded7ef1b271b957480e12833e8d4f0e96558
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-01 22:01:42 +0100

    Create exerciseVariants.js

---
+++ src/data/exerciseVariants.js
@@ -0,0 +1,122 @@
+export const exerciseVariants = [
+
+  {
+    id: "weighted-pull-up",
+
+    name: "Weighted Pull Up",
+
+    baseMovementId: "pull-up",
+
+    modifiers: [
+      "weighted"
+    ]
+  },
+
+  {
+    id: "tempo-pull-up",
+
+    name: "Tempo Pull Up",
+
+    baseMovementId: "pull-up",
+
+    modifiers: [
+      "tempo"
+    ]
+  },
+
+  {
+    id: "pause-pull-up",
+
+    name: "Pause Pull Up",
+
+    baseMovementId: "pull-up",
+
+    modifiers: [
+      "pause"
+    ]
+  },
+
+  {
+    id: "assisted-pull-up",
+
+    name: "Assisted Pull Up",
+
+    baseMovementId: "pull-up",
+
+    modifiers: [
+      "assisted"
+    ]
+  },
+
+  {
+    id: "weighted-dip",
+
+    name: "Weighted Dip",
+
+    baseMovementId: "dip",
+
+    modifiers: [
+      "weighted"
+    ]
+  },
+
+  {
+    id: "tempo-push-up",
+
+    name: "Tempo Push Up",
+
+    baseMovementId: "push-up",
+
+    modifiers: [
+      "tempo"
+    ]
+  },
+
+  {
+    id: "pause-squat",
+
+    name: "Pause Squat",
+
+    baseMovementId: "squat",
+
+    modifiers: [
+      "pause"
+    ]
+  },
+
+  {
+    id: "tempo-squat",
+
+    name: "Tempo Squat",
+
+    baseMovementId: "squat",
+
+    modifiers: [
+      "tempo"
+    ]
+  },
+
+  {
+    id: "single-leg-pogo",
+
+    name: "Single Leg Pogo",
+
+    baseMovementId: "pogo",
+
+    modifiers: [
+      "single-leg"
+    ]
+  },
+
+  {
+    id: "reactive-pogo",
+
+    name: "Reactive Pogo",
+
+    baseMovementId: "pogo",
+
+    modifiers: [
+      "reactive"
+    ]
+  }
+];
