8e92196b5a023d31c628e2508041bb0eb426e458
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-01 20:56:01 +0100

    Create constraintTypes.js

---
+++ src/data/constraintTypes.js
@@ -0,0 +1,29 @@
+export const constraintTypes = [
+
+  "weighted",
+  "assisted",
+  "single-leg",
+  "single-arm",
+  "tempo",
+  "pause",
+  "isometric",
+  "eccentric",
+  "concentric",
+
+  "reactive",
+  "continuous",
+
+  "partial-rom",
+  "full-rom",
+
+  "depth-limited",
+
+  "band-assisted",
+  "band-resisted",
+
+  "unstable",
+
+  "external-load",
+
+  "bodyweight"
+];
